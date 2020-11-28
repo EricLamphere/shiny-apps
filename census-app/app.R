f80aca1f151fc18987129207237855712e820d7a
Eric Lamphere <ericjlamphere@gmail.com>
2020-11-28 16:49:29 -0500

    Initial app commit

---
+++ census-app/app.R
@@ -0,0 +1,8 @@
+### Title:    Census App: Globals & App
+### Author:   Eric Lamphere (ericjlamphere@gmail.com)
+### Time:     2020-11-28 16:33:09
+
+devtools::install_github("EricLamphere/lamps")
+library(shiny); library(lamps)
+
+shiny::runApp('census-app')
---
+++ census-app/server.R
@@ -0,0 +1,21 @@
+### Title:    Census App: Server
+### Author:   Eric Lamphere (ericjlamphere@gmail.com)
+### Time:     2020-11-28 16:33:09
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    output$distPlot <- renderPlot({
+
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+    })
+
+})
