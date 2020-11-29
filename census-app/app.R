1130f9afa2ed2acc46db4205b02728f68d115be1
Eric Lamphere <ericjlamphere@gmail.com>
2020-11-29 18:14:37 -0500

    stopping for now

--- census-app/app.R
+++ census-app/app.R
@@ -10,9 +10,23 @@ library(censusapi)
 # QUERY ####################################################################################################
 ### Census Data: https://api.census.gov/data.html
 ### Getting Started with Census API: https://cran.r-project.org/web/packages/censusapi/vignettes/getting-started.html
-# getCensus(
-#
-# )
+acs1.meta <- listCensusMetadata(
+  name = "acs/acs1",vintage = 2019,
+  type = "variables")
+
+# need to remove the ':!!' from label to use as column names, want to export more functions from lamps first
+sex_age_vars <- acs1.meta %>%
+  filter(group == "B01001") %>%
+  select(name, label) %>%
+  mutate(label = gsub("Estimate!!Total:!!", "", label))
+
+
+census.data <- getCensus(
+  name = "acs/acs1",
+  vintage = 2019,
+  vars =
+)
+
 
 
 
